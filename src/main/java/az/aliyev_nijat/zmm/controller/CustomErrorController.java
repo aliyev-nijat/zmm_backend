2e2b550e3eea06e737bbc3370b4dff03bc2f07cd
aliyev-nijat <nicat3699@gmail.com>
2025-12-09 01:19:56 +0400

    redirect not found page

---
+++ src/main/java/az/aliyev_nijat/zmm/controller/CustomErrorController.java
@@ -0,0 +1,22 @@
+package az.aliyev_nijat.zmm.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import jakarta.servlet.http.HttpServletRequest;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request) {
+        Integer statusCode = (Integer) request.getAttribute("jakarta.servlet.error.status_code");
+        String requestUri = (String) request.getAttribute("jakarta.servlet.error.request_uri");
+
+        if (statusCode != null && statusCode == 404) {
+            return requestUri.startsWith("/adminpanel") ? "redirect:/adminpanel/index.html" : "redirect:/";
+        }
+
+        return "/error.html";
+    }
+}
