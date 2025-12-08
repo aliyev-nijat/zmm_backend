740a9bdef617300b04f17124edc4f11bd3ad96e7
aliyev-nijat <nicat3699@gmail.com>
2025-12-08 12:56:36 +0400

    settings image

--- src/main/java/az/aliyev_nijat/zmm/controller/SettingsController.java
+++ src/main/java/az/aliyev_nijat/zmm/controller/SettingsController.java
@@ -6,11 +6,16 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/settings")
@@ -36,5 +41,13 @@ public class SettingsController {
             @RequestBody SettingsEntity settings
     ) {
         return ResponseEntity.ok(service.update(settings));
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @PostMapping("/image")
+    public ResponseEntity<Map<String, Object>> uploadImage(
+            @RequestParam MultipartFile image
+    ) {
+        return ResponseEntity.ok(service.uploadImage(image));
     }
 }
--- src/main/java/az/aliyev_nijat/zmm/service/SettingService.java
+++ src/main/java/az/aliyev_nijat/zmm/service/SettingService.java
@@ -1,15 +1,26 @@
 package az.aliyev_nijat.zmm.service;
 
+import az.aliyev_nijat.zmm.model.entity.ImageEntity;
 import az.aliyev_nijat.zmm.model.entity.SettingsEntity;
+import az.aliyev_nijat.zmm.repository.ImageRepository;
 import az.aliyev_nijat.zmm.repository.SettingsRepository;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class SettingService {
 
     private final SettingsRepository repository;
+    private final ImageRepository imageRepository;
 
     public SettingsEntity init() {
         SettingsEntity settings = new SettingsEntity();
@@ -25,8 +36,47 @@ public class SettingService {
     public SettingsEntity update(SettingsEntity settings) {
         settings.setId(1L);
         settings.setImageUrl(
-                repository.findById(1L).orElseThrow().getImageUrl()
+                getSettings().getImageUrl()
         );
         return repository.save(settings);
+    }
+
+    public Map<String, Object> uploadImage(
+            MultipartFile image
+    ) {
+        if (image == null || image.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+        SettingsEntity settings = getSettings();
+        if (settings.getImageUrl() != null) {
+            Long oldImageId = Long.valueOf(
+                    settings.getImageUrl()
+                    .substring("/api/images/".length())
+            );
+            imageRepository.delete(oldImageId);
+            settings.setImageUrl(null);
+        }
+        String[] splited = image.getOriginalFilename().split("\\.");
+        String extension = splited[splited.length - 1];
+        byte[] content;
+        try {
+            content = image.getBytes();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        ImageEntity imageEntity = new ImageEntity();
+        imageEntity.setExtension(extension);
+        imageEntity.setContent(content);
+        ImageEntity newImageEntity = imageRepository.create(imageEntity);
+        Long newImageId = newImageEntity.getId();
+        settings.setImageUrl(
+                String.format("/api/images/%d",newImageId)
+        );
+        repository.save(settings);
+        Map<String, Object> result = new HashMap<>();
+        result.put("imageId", newImageId);
+        result.put("imageUrl", String.format("/images/%d", newImageId));
+
+        return result;
     }
 }
